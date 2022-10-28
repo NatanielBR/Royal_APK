53a1870df2ce063259e3f588c5d1dc6c1f0ea330
nataniel <natannada08@gmail.com>
2022-10-28 14:07:34 -0300

    Adicionado função para remover

--- src/Screen/Lista/index.js
+++ src/Screen/Lista/index.js
@@ -1,31 +1,40 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from '@react-navigation/native';
-import React, { useContext, useEffect, useState } from 'react';
-import { Alert, Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native';
-import MoviePoster from '../../Components/MoviePoster';
-import { MyListContext } from '../../Context/MyListContext';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import MoviePoster from "../../Components/MoviePoster";
 import styles from "./styles";
 
-import ImageClose from '../../Assets/Icons/left-arrow.png';
+import ImageClose from "../../Assets/Icons/left-arrow.png";
 
 export default function Lista() {
   const [listStorage, setListStorage] = useState([]);
   const navigation = useNavigation();
 
-
   useEffect(() => {
     async function fetchListStorage() {
-      const myList = await AsyncStorage.getItem('myList');
+      const myList = await AsyncStorage.getItem("myList");
 
       setListStorage(Object.values(JSON.parse(myList)));
     }
+
     fetchListStorage();
   }, []);
+
+  const updateListStorage = async () => {
+    let nList = {};
+
+    listStorage.forEach((item) => {
+      nList[item.Imdbid] = item;
+    });
+
+    await AsyncStorage.setItem("myList", JSON.stringify(nList));
+  };
 
   return (
     <View style={styles.body}>
 
-      <SafeAreaView styles={{ flex: 1, height: 50, width: '100%' }}>
+      <SafeAreaView styles={{ flex: 1, height: 50, width: "100%" }}>
 
         <View style={styles.header}>
           <TouchableOpacity onPress={() => navigation.goBack()} style={styles.btnBack}>
@@ -40,30 +49,39 @@ export default function Lista() {
       </SafeAreaView>
       <ScrollView
         overScrollMode="never">
-        <View style={{ flexDirection: 'row', flex: 1, flexWrap: 'wrap' }}>
+        <View style={{ flexDirection: "row", flex: 1, flexWrap: "wrap" }}>
           {listStorage?.map((movie, index) => {
-            return (
-              <TouchableOpacity
-                key={index}
-                onPress={() => {
-                  navigation.navigate('MovieD', {
-                    Item: movie,
-                    ID: movie.Imdbid,
-                    Type: movie.typeItem
-                  });
-                }}>
-                <MoviePoster
-                  bdS={{ margin: 16 }}
-                  key={movie.Imdbid}
-                  poster={movie.poster}
-                  movieName={movie.title}
-                  rate={movie.ratings}
-                  year={movie.year}
-                  likes={movie.votes}
-                />
-              </TouchableOpacity>
-            );
-          }
+              return (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => {
+                    navigation.navigate("MovieD", {
+                      Item: movie,
+                      ID: movie.Imdbid,
+                      Type: movie.typeItem,
+                    });
+                  }}
+                  onLongPress={async () => {
+                    delete listStorage[index];
+
+                    await updateListStorage();
+
+                    setListStorage([])
+                    setListStorage(listStorage)
+                  }}
+                >
+                  <MoviePoster
+                    bdS={{ margin: 16 }}
+                    key={movie.Imdbid}
+                    poster={movie.poster}
+                    movieName={movie.title}
+                    rate={movie.ratings}
+                    year={movie.year}
+                    likes={movie.votes}
+                  />
+                </TouchableOpacity>
+              );
+            },
           )}
 
 
